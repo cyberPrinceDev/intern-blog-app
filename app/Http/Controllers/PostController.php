e062f4ace7c15e85748b9742ffae940781528c01
devprince <chuksechejunior@gmail.com>
2026-03-04 16:39:03 +0100

    refactor controller with eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -9,52 +10,14 @@ class PostController extends Controller
     //
     public function index()
     {
-        $posts = [
-            [
-                'id' => 1,
-                'title' => 'Getting Started with Laravel',
-                'content' => 'Learn the basics of Laravel framework and build your first application.',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Understanding Eloquent ORM',
-                'content' => 'Dive deeper into Laravel\'s Eloquent ORM and learn how to work with databases efficiently.',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Building RESTful APIs',
-                'content' => 'A comprehensive guide to building RESTful APIs using Laravel with proper routing and controllers.',
-            ],
-        ];
+        $posts = Post::all();
 
         return view('posts.index', ['posts' => $posts]);
     }
 
     public function show($id)
     {
-        $posts = [
-            [
-                'id' => 1,
-                'title' => 'Getting Started with Laravel',
-                'content' => 'Learn the basics of Laravel framework and build your first application.',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Understanding Eloquent ORM',
-                'content' => 'Dive deeper into Laravel\'s Eloquent ORM and learn how to work with databases efficiently.',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Building RESTful APIs',
-                'content' => 'A comprehensive guide to building RESTful APIs using Laravel with proper routing and controllers.',
-            ],
-        ];
-
-        $post = collect($posts)->firstWhere('id', $id);
-
-        if (!$post) {
-            return redirect('/posts')->with('error', 'Post not found');
-        }
+        $post = Post::findOrFail($id);
 
         return view('posts.show', ['post' => $post]);
     }
