5873b154d2183faefecf5b57613e1a110aa5a9a4
devprince <chuksechejunior@gmail.com>
2026-03-05 09:54:46 +0100

    displayed errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="container">
         <h1>Create New Post</h1>
         <form action="{{ route('posts.store') }}" method="POST">
